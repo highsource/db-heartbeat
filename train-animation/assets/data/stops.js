7c1d067483b777dc21106b7e86e243ecdc1a930a
danielt <daniel.teske@gmail.com>
2017-05-13 00:00:49 +0200

    train animation

---
+++ train-animation/assets/data/stops.js
@@ -0,0 +1,172 @@
+var stops = {
+  "features" : [ {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 9.181635, 48.784084 ]
+    },
+    "properties" : {
+      "name" : "Stuttgart Hbf",
+      "evaNr" : "8000096",
+      "ds100" : "TS"
+    },
+    "name" : "Stuttgart Hbf",
+    "evaNr" : "8000096",
+    "ds100" : "TS",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 8.468921, 49.479354 ]
+    },
+    "properties" : {
+      "name" : "Mannheim Hbf",
+      "evaNr" : "8000244",
+      "ds100" : "RM"
+    },
+    "name" : "Mannheim Hbf",
+    "evaNr" : "8000244",
+    "ds100" : "RM",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 8.663789, 50.107145 ]
+    },
+    "properties" : {
+      "name" : "Frankfurt(Main)Hbf",
+      "evaNr" : "8000105",
+      "ds100" : "FF"
+    },
+    "name" : "Frankfurt(Main)Hbf",
+    "evaNr" : "8000105",
+    "ds100" : "FF",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 8.929, 50.120953 ]
+    },
+    "properties" : {
+      "name" : "Hanau Hbf",
+      "evaNr" : "8000150",
+      "ds100" : "FH  N"
+    },
+    "name" : "Hanau Hbf",
+    "evaNr" : "8000150",
+    "ds100" : "FH  N",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 9.683977, 50.554723 ]
+    },
+    "properties" : {
+      "name" : "Fulda",
+      "evaNr" : "8000115",
+      "ds100" : "FFU"
+    },
+    "name" : "Fulda",
+    "evaNr" : "8000115",
+    "ds100" : "FFU",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 9.446898, 51.313114 ]
+    },
+    "properties" : {
+      "name" : "Kassel-Wilhelmshöhe",
+      "evaNr" : "8003200",
+      "ds100" : "FKW"
+    },
+    "name" : "Kassel-Wilhelmshöhe",
+    "evaNr" : "8003200",
+    "ds100" : "FKW",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 9.926072, 51.536815 ]
+    },
+    "properties" : {
+      "name" : "Göttingen",
+      "evaNr" : "8000128",
+      "ds100" : "HG"
+    },
+    "name" : "Göttingen",
+    "evaNr" : "8000128",
+    "ds100" : "HG",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 9.953497, 52.160626 ]
+    },
+    "properties" : {
+      "name" : "Hildesheim Hbf",
+      "evaNr" : "8000169",
+      "ds100" : "HHI"
+    },
+    "name" : "Hildesheim Hbf",
+    "evaNr" : "8000169",
+    "ds100" : "HHI",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 10.540295, 52.252218 ]
+    },
+    "properties" : {
+      "name" : "Braunschweig Hbf",
+      "evaNr" : "8000049",
+      "ds100" : "HBS"
+    },
+    "name" : "Braunschweig Hbf",
+    "evaNr" : "8000049",
+    "ds100" : "HBS",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 13.197534, 52.534475 ]
+    },
+    "properties" : {
+      "name" : "Berlin-Spandau",
+      "evaNr" : "8010404",
+      "ds100" : "BSPD"
+    },
+    "name" : "Berlin-Spandau",
+    "evaNr" : "8010404",
+    "ds100" : "BSPD",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 13.369545, 52.525592 ]
+    },
+    "properties" : {
+      "name" : "Berlin Hbf",
+      "evaNr" : "8011160",
+      "ds100" : "BLS"
+    },
+    "name" : "Berlin Hbf",
+    "evaNr" : "8011160",
+    "ds100" : "BLS",
+    "type" : "Feature"
+  }, {
+    "geometry" : {
+      "type" : "Point",
+      "coordinates" : [ 13.434564, 52.510977 ]
+    },
+    "properties" : {
+      "name" : "Berlin Ostbahnhof",
+      "evaNr" : "8010255",
+      "ds100" : "BHF"
+    },
+    "name" : "Berlin Ostbahnhof",
+    "evaNr" : "8010255",
+    "ds100" : "BHF",
+    "type" : "Feature"
+  } ],
+  "type" : "FeatureCollection"
+}
---
+++ train-animation/js/zrs.js
@@ -0,0 +1,106 @@
+$(function() {
+    // Setup leaflet map
+    var map = new L.Map('map');
+
+    var basemapLayer = new L.TileLayer('https://{s}.tiles.mapbox.com/v3/github.map-xgq2svrz/{z}/{x}/{y}.png',
+			{attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery &copy; <a href="http://mapbox.com">Mapbox</a>'});
+
+    // Center map and default zoom level
+    map.setView([51.5, 10.5], 6);
+
+
+    // Adds the background layer to the map
+    map.addLayer(basemapLayer);
+
+    // Colors for AwesomeMarkers
+    var _colorIdx = 0,
+        _colors = [
+          'blue',
+          'red',
+          'orange',
+          'green',
+          'purple',
+          'darkred',
+          'cadetblue',
+          'darkgreen',
+          'darkblue',
+          'darkpurple'
+        ];
+        
+    function _assignColor() {
+        return _colors[_colorIdx++%10];
+    }
+    
+    // =====================================================
+    // =============== Playback ============================
+    // =====================================================
+
+	L.geoJson(lines, {
+		style: function (feature) {
+			return {color: 'black', weight:3, opacity: 1};
+    		}
+	}).addTo(map);
+	L.geoJson(lines, {
+		style: function (feature) {
+			return {color: 'white', weight:2, opacity: 1, dashArray : '3, 3'};
+    		}
+	}).addTo(map);
+
+	L.geoJson(stops, {
+		pointToLayer: function (feature, latlng) {
+			return L.circleMarker(latlng, {
+					radius: 6,
+					fillColor: 'white',
+					color: 'black',
+					weight: 2,
+					opacity: 1,
+					fillOpacity: 1
+				}).bindLabel(feature.properties.name + "<br/>(" + feature.properties.ds100 + " / " + feature.properties.evaNr + ")", { direction: 'auto' });
+			}
+	}).addTo(map);
+
+
+    // Playback options
+    var playbackOptions = {        
+        speed: 512,       
+        // layer and marker options
+        layer: {
+            pointToLayer : function(featureData, latlng){
+                var result = {};
+                
+                if (featureData && featureData.properties && featureData.properties.path_options){
+                    result = featureData.properties.path_options;
+                }
+                
+                if (!result.radius){
+                    result.radius = 5;
+                }
+                
+                return new L.CircleMarker(latlng, result);
+            }
+        },
+        
+        marker: function(){
+            return {
+                icon: L.AwesomeMarkers.icon({
+                    prefix: 'fa',
+                    icon: 'train', 
+                    markerColor: _assignColor()
+                }) 
+            };
+        }        
+    };
+    
+    // Initialize playback
+    playback = new L.Playback(map, tracks, null, playbackOptions);
+    playback.setSpeed(512);
+    // Initialize custom control
+    var control = new L.Playback.Control(playback);
+    control.addTo(map);
+
+    playback.setCursor(46260000);
+    playback.setSpeed(512);
+    $('.speed').html(512).val(512);
+});
